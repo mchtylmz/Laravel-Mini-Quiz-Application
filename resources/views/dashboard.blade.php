ab2d7a560f48a20780ecb85c31077e1276a821b9
mchtylmz <mchtylmz149@gmail.com>
2021-09-13 13:57:01 +0300

    jetstream, bootstrap eklendi, ilk ayarlar yapıldı

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,7 @@
+<x-app-layout>
+    <x-slot name="header">{{ __('Dashboard') }}</x-slot>
+
+    <div class="bg-dangera overflow-hidden shadow-xl sm:rounded-lg">
+
+    </div>sodium_base642bins
+</x-app-layout>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ $header }} - {{ config('app.name', 'LaraQuiz') }}</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+        @livewireStyles
+
+        <!-- Scripts -->
+        <script src="{{ mix('js/app.js') }}" defer></script>
+    </head>
+    <body class="font-sans antialiased">
+        <x-jet-banner />
+
+        <div class="min-h-screen bg-gray-100">
+            @livewire('navigation-menu')
+
+            <!-- Page Content -->
+            <div class="py-6">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    {{ $slot }}
+                </div>
+            </div>
+        </div>
+
+        @stack('modals')
+
+        @livewireScripts
+    </body>
+</html>
